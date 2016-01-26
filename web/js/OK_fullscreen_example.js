7d0320d08a55f4cdd8fba8fe58cd71c7dda35cb7
tibyandy <tibyandy@gmail.com>
2016-01-26 00:01:41 -0200

    fix(iOS): fullscreen web app

--- web/js/OK_fullscreen_example.js
+++ web/js/OK_fullscreen_example.js
@@ -1,10 +1,15 @@
 (function (D) {
 	var toggler = D.getElementById('fs_toggler');
 
-	toggler.onclick = function () {
+	var ontap = typeof document.documentElement.ontouchend != 'undefined' ? 'ontouchend' : 'onclick';
+
+	toggler[ontap] = function () {
+		navigator.vibrate = navigator.vibrate || navigator.webkitVibrate || navigator.mozVibrate || navigator.msVibrate;
 		if (!OK.fullscreen.isActive()) {
+	  		navigator.vibrate([30, 10, 30]);
 			OK.fullscreen.turnOn();
 		} else {
+	  		navigator.vibrate([30]);
 			OK.fullscreen.turnOff();
 		}
 	};
@@ -16,4 +21,14 @@
 	OK.fullscreen.onExit(function () {
 		toggler.innerHTML = 'Fullscreen OFF!<br>Click to enter fullscreen';
 	});
+
+	if (window.matchMedia('(display-mode: standalone)').matches) {
+	    toggler.innerHTML = 'App in standalone mode! Fullscreen on ';
+	}
+
+	if (window.navigator.standalone) {
+		toggler.innerHTML = 'App in iOS standalone mode!';
+	} else {
+		toggler.innerHTML = toggler.innerHTML + ontap;
+	}
 })(document);
